ef8f7df192f99a057acb69495d833cad17abc3c2
Pablo Martinez <martinezpablo06@gmail.com>
2019-05-16 20:07:42 -0300

    add comprobateActive

--- src/main/java/trivia/Question.java
+++ src/main/java/trivia/Question.java
@@ -7,5 +7,25 @@ public class Question extends Model {
 	static{//para que no permita cargar vacios estos campos
 		validatePresenceOf("category_id", "description");
 	}
+	
+	public boolean comprobateActive (){
+
+	    if(this!=null){
+		    List<Option> options= Option.where("question_id = ?", this.get("id"));
+            int correctas = 0;
+            int totales = 0;
+
+		    for(Option o : options){
+                
+                if ( o.getBoolean("correct")) {
+                    corectas ++;                    
+                }
+                totales ++;
+		    }
+        return ((correctas ==1)&&(totales == 4));
+        }
+        
+       return false;
+     }
 
 }
