1c57f53d263fc4b3019863106c14569a6e1839c9
cuestalvaro <alvarosergiocuesta@gmail.com>
2019-09-29 21:47:28 -0300

    cambio visuales estaditicas por categoria y mostrando con porcentaje

--- client/screens/CategStatScreen.js
+++ client/screens/CategStatScreen.js
@@ -51,12 +51,37 @@ export default class CategStatScreen extends React.Component {
 // Correctas: {JSON.stringify(c1.correct)}
 //Incorrectas: {JSON.stringify(c1.incorrect)}
   render() {
+
+    function dosDecimales(n) {
+      let t=n.toString();
+      let regex=/(\d*.\d{0,2})/;
+      return t.match(regex)[0];
+    }
+
+    function calcularPorcentajeCorrrectas(item) {
+      var totalPreg= parseInt(item.correct)+parseInt(item.incorrect);
+      var porcentajeCorrectas= dosDecimales(item.correct*100/totalPreg);
+      if (porcentajeCorrectas==0 || totalPreg==0){
+        return 0;
+      }
+      return porcentajeCorrectas;
+    }
+
+    function calcularPorcentajeIncorrrectas(item) {
+      var totalPreg= parseInt(item.correct)+parseInt(item.incorrect);
+      var porcentajeIncorrectas= dosDecimales(item.incorrect*100/totalPreg);
+      if (porcentajeIncorrectas==0 || totalPreg==0){
+        return 0;
+      }
+      return porcentajeIncorrectas;
+    }
+
       const c= this.state.cats;
       const cant=this.state.cant;
-      console.log(c);
-      console.log(cant);
-      
+      //console.log(c);
+      //console.log(cant);
      // console.log(cat);
+     
       return(
         <View style={styles.container}>
           <FlatList          
@@ -69,9 +94,11 @@ export default class CategStatScreen extends React.Component {
                     <Text style={styles.categoryStyle}>
                       {item.nombre} {"\n"}
                     </Text>
-                    <Text style={styles.getStartedText}>
-                      Correctas:  {" "} {item.correct} {" "}
-                      Incorrectas:  {" "} {item.incorrect}
+                    <Text style={styles.correctas}>
+                      Correctas:  {" "} {calcularPorcentajeCorrrectas(item)+'%'} {" "}
+                      <Text style={styles.incorrectas}>
+                        Incorrectas:  {" "} {calcularPorcentajeIncorrrectas(item)+'%'}
+                      </Text>
                     </Text>
                   </View>
                 </ScrollView>
@@ -93,21 +120,25 @@ const styles = StyleSheet.create({
       paddingTop: 30,
     },
     categoryStyle: {
-        fontSize:20, 
-        color:'#ffff00', 
+        fontSize:25, 
+        color:'#663399', 
         paddingLeft:30, 
         paddingRight:30, 
-        textShadowColor:'#585858', 
-        textShadowOffset:{width: 5, height: 5}, 
-        textShadowRadius:10, 
+     
     },
     getStartedContainer: {
       alignItems: 'center',
       marginHorizontal: 50,
     },
-    getStartedText: {
+    correctas: {
       fontSize: 17,
-      color: 'rgba(255, 255, 255, 1)',
+      color: '#32cd32',
+      lineHeight: 24,
+      textAlign: 'center',
+    },
+    incorrectas: {
+      fontSize: 17,
+      color: '#ff0000',
       lineHeight: 24,
       textAlign: 'center',
     },
