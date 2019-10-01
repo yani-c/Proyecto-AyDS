204713a5335cdda81ed306dc42cc8a1e67e4246c
cuestalvaro <alvarosergiocuesta@gmail.com>
2019-10-01 19:02:40 -0300

    modificacion del modelo Level.java

--- src/main/java/trivia/Level.java
+++ src/main/java/trivia/Level.java
@@ -4,4 +4,9 @@ import org.javalite.activejdbc.Model;
 
 public class Level extends Model {
 
+
+    static{//para que no permita cargar vacios estos campos
+		validatePresenceOf("level","category_id", "user_id");
+	}
+
 }
